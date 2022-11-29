d16dac41bcbbd957690cf8f5515824f8cbe4b6d2
kakon-mehedi <kakon9000@gmail.com>
2022-11-30 00:24:05 +0600

    Complete UI

---
+++ text.js
@@ -0,0 +1,18 @@
+Cras scelerisque id quam sed dignissim Pellentesque urna nunc, gravida quis hendrerit ac, tristique ut quam. Vivamus suscipit dignissim tortor nec congue. 
+
+
+
+
+
+
+
+25 Nov 2016		Vestibulum viverra
+28 Nov 2016		Vestibulum viverra
+
+18 Dec 2016		Vestibulum viverra
+
+7 Jan 2017		Vestibulum viverra
+
+
+
+SCHEULE FONT COLOR #414f6b
